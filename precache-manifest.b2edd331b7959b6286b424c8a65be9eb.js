0ca817fa331efc1d44f84879e613fadfde94d3bd
santoshi rakshita <rakshita1999>
2019-06-04 17:04:13 +0530

    Updates

---
+++ precache-manifest.b2edd331b7959b6286b424c8a65be9eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab66a2b9db53e90593bc08021c792e28",
+    "url": "/unsplash-app/index.html"
+  },
+  {
+    "revision": "8d8c33fae8457800101c",
+    "url": "/unsplash-app/static/css/main.707e1302.chunk.css"
+  },
+  {
+    "revision": "a701f1d3c507b4ad33ed",
+    "url": "/unsplash-app/static/js/2.34eb5d5b.chunk.js"
+  },
+  {
+    "revision": "8d8c33fae8457800101c",
+    "url": "/unsplash-app/static/js/main.56f4efab.chunk.js"
+  },
+  {
+    "revision": "e78f17c2145982b11f8e",
+    "url": "/unsplash-app/static/js/runtime~main.918bf6fc.js"
+  }
+]);
